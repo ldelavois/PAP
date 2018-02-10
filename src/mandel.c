6f3ac7022dfd9231e8433d71358aa65b48cfd5d9
julmass <jul.massonneau@gmail.com>
2018-02-10 15:28:25 +0100

    correction

--- src/mandel.c
+++ src/mandel.c
@@ -181,7 +181,7 @@ unsigned mandel_compute_omps (unsigned nb_iter)
 {
 
   for (unsigned it = 1; it <= nb_iter; it ++) {
-    #pragma parallel omp for schedule(static)
+    #pragma omp parallel for schedule(static)
     for (int i = 0; i < DIM; i++)
       for (int j = 0; j < DIM; j++)
 	cur_img (i, j) = iteration_to_color (compute_one_pixel (i, j));
@@ -197,7 +197,7 @@ unsigned mandel_compute_ompd (unsigned nb_iter)
 {
 
   for (unsigned it = 1; it <= nb_iter; it ++) {
-    #pragma parallel omp for schedule(dynamic)
+    #pragma omp parallel for schedule(dynamic)
     for (int i = 0; i < DIM; i++)
       for (int j = 0; j < DIM; j++)
 	cur_img (i, j) = iteration_to_color (compute_one_pixel (i, j));
